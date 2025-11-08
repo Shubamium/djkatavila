f3c2a664d074d538fb9629d5733e1bb86580e129
Vincent_Liussyaputra <liusvinv@gmail.com>
2025-11-08 18:22:07 +0700

    Add snapchat and linktree

--- src/app/(katavila)/(components)/footer/Footer.tsx
+++ src/app/(katavila)/(components)/footer/Footer.tsx
@@ -1,34 +1,74 @@
-import Link from 'next/link';
-import './footer.scss';
-export async function Footer(){
-	return <footer id="footer">
-		<img src="/d/footer-circ-l.png" alt="" className='footer-circ l' />
-		<img src="/d/footer-circ-r.png" alt="" className='footer-circ r' />
-		<img src="/g/logo_f.png" alt="" className='logo' />
-		<div className="socials">
-					<a className={'btn-sc btn'} target="_blank" href="https://www.instagram.com/djkatavila">
-						<img src="/g/ic_ig.png" alt="" className={'icon'} />
-					</a>
-					<a className={'btn-sc btn'} target="_blank" href="https://www.tiktok.com/@djkatavila" >
-						<img src="/g/ic_tt.png" alt="" className={'icon'} />
-					</a>
-					<a className={'btn-sc btn'} target="_blank" href="https://www.youtube.com/@djkatavila/videos">
-						<img src="/g/ic_yt.png" alt="" className={'icon'} />
-					</a>
-<a className={'btn-sc btn'}target="_blank"  href="https://open.spotify.com/user/317z7s5hzcvddqdpamiogvzidaam?si=f59d2be0540f42dc">
-						<img src="/g/ic_sp.png" alt="" className={'icon'} />
-					</a>
-					<a className={'btn-sc btn'} target="_blank" href="https://www.mixcloud.com/katavila">
-						<img src="/g/ic_mc.png" alt="" className={'icon'} />
-					</a>
-		</div>
-		<nav id='fnav'>
-			<Link href={'/'} className='btn'> ABOUT</Link>
-			<Link href={'/schedule'} className='btn'> SCHEDULE</Link>
-			<Link href={'/booking'} className='btn'> BOOKING</Link>
-		</nav>
-		<a href='https://x.com/shubamium2' className="attrib">
-			<p>website design by <span>SHUBAMIUM</span></p>
-		</a>
-	</footer>
+import Link from "next/link";
+import "./footer.scss";
+export async function Footer() {
+  return (
+    <footer id="footer">
+      <img src="/d/footer-circ-l.png" alt="" className="footer-circ l" />
+      <img src="/d/footer-circ-r.png" alt="" className="footer-circ r" />
+      <img src="/g/logo_f.png" alt="" className="logo" />
+      <div className="socials">
+        <a
+          className={"btn-sc btn"}
+          target="_blank"
+          href="https://www.instagram.com/djkatavila"
+        >
+          <img src="/g/ic_ig.png" alt="" className={"icon"} />
+        </a>
+        <a
+          className={"btn-sc btn"}
+          target="_blank"
+          href="https://www.tiktok.com/@djkatavila"
+        >
+          <img src="/g/ic_tt.png" alt="" className={"icon"} />
+        </a>
+        <a
+          className={"btn-sc btn"}
+          target="_blank"
+          href="https://www.youtube.com/@djkatavila/videos"
+        >
+          <img src="/g/ic_yt.png" alt="" className={"icon"} />
+        </a>
+        <a
+          className={"btn-sc btn"}
+          target="_blank"
+          href="https://open.spotify.com/user/317z7s5hzcvddqdpamiogvzidaam?si=f59d2be0540f42dc"
+        >
+          <img src="/g/ic_sp.png" alt="" className={"icon"} />
+        </a>
+        <a
+          className={"btn-sc btn"}
+          target="_blank"
+          href="https://www.mixcloud.com/katavila"
+        >
+          <img src="/g/ic_mc.png" alt="" className={"icon"} />
+        </a>
+        <a
+          className={"btn-sc btn"}
+          target="_blank"
+          href="https://www.snapchat.com/add/djkatavila?share_id=2mONGScsSNOYu8E7CnV8OQ&locale=en_US"
+        >
+          <img src="/g/ic_sc.png" alt="" className={"icon"} />
+        </a>
+      </div>
+      <nav id="fnav">
+        <Link href={"/"} className="btn">
+          {" "}
+          ABOUT
+        </Link>
+        <Link href={"/schedule"} className="btn">
+          {" "}
+          SCHEDULE
+        </Link>
+        <Link href={"/booking"} className="btn">
+          {" "}
+          BOOKING
+        </Link>
+      </nav>
+      <a href="https://x.com/shubamium2" className="attrib">
+        <p>
+          website design by <span>SHUBAMIUM</span>
+        </p>
+      </a>
+    </footer>
+  );
 }
--- src/app/(katavila)/(components)/header/Header.tsx
+++ src/app/(katavila)/(components)/header/Header.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import React, { useState } from "react";
 
 type Props = {};
@@ -6,7 +6,7 @@ import "./header.scss";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 export default function Header({}: Props) {
-	const pathname = usePathname();
+  const pathname = usePathname();
   return (
     <>
       <header id="header">
@@ -16,14 +16,65 @@ export default function Header({}: Props) {
         <img src="/d/palm.png" alt="" className="palm r" />
         <img src="/d/wave.png" alt="" className="wave l" />
         <img src="/d/wave.png" alt="" className="wave r" />
+        <div className="linktree">
+          <a
+            className={"btn-sc btn"}
+            target="_blank"
+            href="https://www.instagram.com/djkatavila"
+          >
+            <img src="/g/ic_ig.png" alt="" className={"icon"} />
+            <span>INSTAGRAM</span>
+          </a>
+          <a
+            className={"btn-sc btn"}
+            target="_blank"
+            href="https://www.tiktok.com/@djkatavila"
+          >
+            <img src="/g/ic_tt.png" alt="" className={"icon"} />
+            <span>TIKTOK</span>
+          </a>
+          <a
+            className={"btn-sc btn"}
+            target="_blank"
+            href="https://www.youtube.com/@djkatavila/videos"
+          >
+            <img src="/g/ic_yt.png" alt="" className={"icon"} />
+            <span>YOUTUBE</span>
+          </a>
+          <a
+            className={"btn-sc btn"}
+            target="_blank"
+            href="https://open.spotify.com/user/317z7s5hzcvddqdpamiogvzidaam?si=f59d2be0540f42dc"
+          >
+            <img src="/g/ic_sp.png" alt="" className={"icon"} />
+            <span>TIKTOK</span>
+          </a>
+          <a
+            className={"btn-sc btn"}
+            target="_blank"
+            href="https://www.snapchat.com/add/djkatavila?share_id=2mONGScsSNOYu8E7CnV8OQ&locale=en_US"
+          >
+            <img src="/g/ic_sc.png" alt="" className={"icon"} />
+            <span>SNAPCHAT</span>
+          </a>
+        </div>
         <nav>
-          <Link href="/" className={`btn btn-main ${pathname === '/' ? 'active' : ''}`}>
+          <Link
+            href="/"
+            className={`btn btn-main ${pathname === "/" ? "active" : ""}`}
+          >
             ABOUT
           </Link>
-          <Link href="/schedule" className={`btn btn-main ${pathname.includes('/schedule') ? 'active' : ''}`}>
+          <Link
+            href="/schedule"
+            className={`btn btn-main ${pathname.includes("/schedule") ? "active" : ""}`}
+          >
             SCHEDULE
           </Link>
-          <Link href="/booking" className={`btn btn-main ${pathname.includes('/booking') ? 'active' : ''}`}>
+          <Link
+            href="/booking"
+            className={`btn btn-main ${pathname.includes("/booking") ? "active" : ""}`}
+          >
             BOOKING
           </Link>
         </nav>
